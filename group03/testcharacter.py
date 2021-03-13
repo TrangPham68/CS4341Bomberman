2bd50e028813dab58bd2ed630f0a6df96251930d
Gwyneth Ormes <gormes@wpi.edu>
2021-03-13 14:50:10 -0500

    Pathfinding is sooo slow

--- group03/testcharacter.py
+++ group03/testcharacter.py
@@ -10,6 +10,103 @@ sys.path.insert(0, '../bomberman')
 from entity import CharacterEntity
 from colorama import Fore, Back
 
+
+class Node():
+    """A node class for A* Pathfinding"""
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+def astar(maze, start, end):
+    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+
+    # Create start and end node
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Add the start node
+    open_list.append(start_node)
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+
+        # Get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
+
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # Add the child to the open list
+            open_list.append(child)
+
 class TestCharacter(CharacterEntity):
     def __init__(self, name, player, x, y):
         CharacterEntity.__init__(self, name, player, x, y)
@@ -21,9 +118,9 @@ class TestCharacter(CharacterEntity):
     def do(self, wrld):
         # Your code here
         print (self.x, " ", self.y)
-        if self.exit is None:
-            x, y = self.find_exit(wrld)
-        path = self.pathfinding((self.x, self.y), (x,y), wrld) #hard code end point
+        # if self.exit is None:
+        #     x, y = self.find_exit(wrld)
+        # path = self.pathfinding((self.x, self.y), (x,y), wrld) #hard code end point
         #
         #if (len(path) > 1) and len(self.find_monsters(wrld)) == 0:
         #      self.move(path[1][0] - self.x, path[1][1] - self.y)
@@ -31,16 +128,29 @@ class TestCharacter(CharacterEntity):
         #     move = self.q_learn(wrld, self.x, self.y)
         #     self.update_q(wrld, self.x, self.y)
         grid = self.build_grid(wrld)
+        print(grid)
         move = self.expectimax(wrld, grid, self.x, self.y, 0, wrld.time)[0]
         self.move(move[0]-self.x, move[1]-self.y)
 
     #TODO: COMMENT
-    def expectimax(self, world, grid, x, y, depth, time):
+    def expectimax(self, world, map, x, y, depth, time):
+
+        grid = map[0]
+        exit = map[1]
+
+        st = (y,x)
+        end = (18,7)
+
+        print(grid)
+        print(st)
+        print(end)
+
+
         print("\nStart Expectimax at")
         print(x,y)
         #TODO: DEPTH MULTIPLIER
         # TODO: CHANGE TO ACTUAL EXIT
-        path = self.pathfinding((x, y), (7,18), world)
+        path = astar(grid, st, end)
         value = -len(path)
         print("To Exit")
         print(value)
@@ -48,8 +158,8 @@ class TestCharacter(CharacterEntity):
             return (x,y,0)
 
         print("To Monster")
-        print(self.distance_to_monster(world, x, y))
-        value -= (self.distance_to_monster(world, x, y))
+        print(self.distance_to_monster(grid, world, x, y))
+        value -= (self.distance_to_monster(grid, world, x, y))
 
         if depth == self.maxdepth or (x,y)==self.find_exit(world):
             return ((x,y), value)
@@ -175,6 +285,7 @@ class TestCharacter(CharacterEntity):
         print(world.height())
         print(world.width())
         arr = []
+        e=(-1,-1)
         for x in range(rows):
             col = []
             for y in range(cols):
@@ -188,14 +299,13 @@ class TestCharacter(CharacterEntity):
                     col.append(4)
                 elif world.explosion_at(y, x):
                     col.append(5)
+                elif world.exit_at(y, x):
+                    e = (y,x)
+                    col.append(0)
                 else:
                     col.append(0)
             arr.append(col)
-        return arr
-
-    def get_heuristic(self, start, end):  # for now just compute distance
-        """return the heuristic value from start point to end point"""
-        return self.get_distance(start, end)  
+        return (arr, e)
 
     def get_distance(self, start, end):  # compute distance
         """return Manhatten distance from start to end point"""
@@ -210,7 +320,7 @@ class TestCharacter(CharacterEntity):
         path = []
 
         while (not current == startNode):
-            path.insert(0, current.getNodePos())
+            path.insert(0, current.gtNodePos())
             current = current.getParent()
 
         path.insert(0, startNode.getNodePos())
@@ -244,9 +354,6 @@ class TestCharacter(CharacterEntity):
 
         return neighbor
 
-    def create_node(self, pos):
-        """create a Node of a specific location"""
-        return node.Node(pos)
 
     def find_exit(self, wrld): 
         """finds the position of the exit"""
@@ -255,21 +362,21 @@ class TestCharacter(CharacterEntity):
                 if wrld.exit_at(x,y):
                     return x,y
 
-    def find_monsters(self,wrld):
+    def find_monsters(self, grid, width, height):
         """finds position of nearest monster in world"""
         monsters = []
 
-        for x in range(wrld.width()):
-            for y in range(wrld.height()):
-                if wrld.monsters_at(x,y):
+        for x in range(height):
+            for y in range(width):
+                if grid[x][y] == 2:
                     monsters.append((x,y))
-        
+        print(monsters)
         return monsters
 
     #These are our features I think
-    def distance_to_monster(self, wrld, x, y):
+    def distance_to_monster(self, grid, wrld, x, y):
         """check position of monster relative to character"""
-        monsters = self.find_monsters(wrld) 
+        monsters = self.find_monsters(grid, wrld.width(), wrld.height())
 
         if len(monsters) == 0:
             return 0 
@@ -280,92 +387,9 @@ class TestCharacter(CharacterEntity):
             d2 = self.get_distance((x,y), closest_m)
             if d1 < d2:
                 closest_m = monster 
-
-        path = self.pathfinding((x,y), closest_m, wrld)
+        print("WHAT")
+        path = astar(grid, closest_m, (x,y))
         length = len(path)
         if length == 0:
             return 1
         return length
-
-    def distance_to_exit(self, wrld, x, y):
-        """check distance to exit"""
-
-        exit_loc = self.find_exit(wrld) 
-        path = self.pathfinding((x,y), exit_loc, wrld)
-        length = len(path)
-        if length == 0:
-            return 1
-        return 1/(length**2)
-
-    def sum_rewards(self, wrld, x, y): 
-        if wrld.exit_at(x, y):
-            return wrld.time * 2 
-        elif wrld.monsters_at(x,y) or wrld.explosion_at(x,y) or wrld.bomb_at(x,y):
-            return -100
-        return 1 
-
-    def extract_features(self, wrld, x, y):
-        f1 = self.distance_to_monster(wrld, x, y)
-        f2 = self.distance_to_exit(wrld, x, y)
-        #NEed to add more
-        return [f1, f2]
-    
-    def q_value(self, wrld, x, y):
-        fvec = self.extract_features(wrld, x, y)
-        vals = list()
-        i = 0
-        for f in fvec:
-            new_w = self.weights[i] * fvec[i]
-            vals.append(new_w) 
-            i += 1
-        q = sum(vals)
-        return q
-    
-    def next_best_q(self, wrld):
-        neighbors = self.get_neighbors((self.x, self.y), wrld) 
-        direction = [] 
-
-        for n in neighbors:
-            if wrld.empty_at(n[0], n[1]):
-                direction.append((n[0] - self.x, n[1] - self.y)) 
-        
-        q_table = []
-        for actions in direction:
-            q_table.append(self.q_value(wrld, self.x, self.y))
-        max_q = max(q_table) 
-        return max_q
-
-    def q_learn(self, wrld, x, y): 
-        max_q = -999
-        max_a = (0,0)
-
-        neighbors = self.get_neighbors((self.x, self.y), wrld) 
-        direction = [] 
-        for n in neighbors:
-            if wrld.empty_at(n[0], n[1]):
-                direction.append((n[0] - self.x, n[1] - self.y)) 
-        
-        for move in direction:
-            new_pos = x + move[0], y + move[1] 
-            wrld.me(self).move(move[0], move[1])
-            new_wrld, events = wrld.next()
-            new_q = self.q_value(new_wrld, new_pos[0], new_pos[1])
-
-            if new_q > max_q:
-                max_q = new_q
-                max_a = move
-
-            return max_a
-
-    def update_q(self, wrld, x, y): 
-        fvec = self.extract_features(wrld, x, y)
-        q = self.q_value(wrld, x, y)
-        next_q = self.next_best_q(wrld)
-        reward = self.sum_rewards(wrld, x, y)
-        diff = reward + self.discount_factor * next_q - q
-        i = 0 
-        for f in fvec: 
-            weight = self.weights[i] 
-            updated_q = weight + self.learning_rate * diff * fvec[i]
-            self.weights[i] = updated_q
-            i += 1
